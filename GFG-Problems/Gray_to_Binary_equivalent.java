54eb650290996e85985edb9dbb9eaa10ce61e001
Unnikrishnan.c <unnikrishnan131100@gmail.com>
2024-04-11 13:11:51 +0530

    11-04-2024-V2

--- GFG-Problems/Gray_to_Binary_equivalent.java
+++ GFG-Problems/Gray_to_Binary_equivalent.java
@@ -9,10 +9,26 @@ public class Gray_to_Binary_equivalent {
         }
         return ans;
     }
+    public int Method2(int n){
+        String s = Integer.toBinaryString(n);
+        String ans = s.charAt(0)+"";
+        
+        for(int i=1; i<s.length(); i++){
+            char ch1 = s.charAt(i);
+            char ch2 = ans.charAt(i-1);
+            if(ch2!=ch1){
+                ans = ans+"1";
+            }else{
+                ans = ans+"0";
+            }
+        }
+        return Integer.parseInt(ans,2);
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         Gray_to_Binary_equivalent solution=new Gray_to_Binary_equivalent();
-        System.out.println("Method 1: "+solution.Method1(n));
+        // System.out.println("Method 1: "+solution.Method1(n));
+        System.out.println("Method 2: "+solution.Method2(n));
     }    
 }
